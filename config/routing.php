856750d611e83f3ef487f3f476b0e8453bceee53
whoisthecoon <remi@cazalet.net>
2013-09-23 00:56:39 +0200

    clean up old routes from a copied file

--- config/routing.php
+++ config/routing.php
@@ -8,32 +8,6 @@ return array(
     ),
     'routes' => array( 
         array(
-            'url'    => '/', 
-            'target' => array( 'controller' => 'main', 'action' => 'index' ) 
-        ),
-        array(
-            'url'    => '/telecharger.html', 
-            'target' => array( 'controller' => 'main', 'action' => 'download' ) 
-        ),
-        array(
-            'url'    => '/a-propos.html', 
-            'target' => array( 'controller' => 'main', 'action' => 'about' ) 
-        ),
-        array(
-            'url'    => '/docs/',
-            'target' => array( 'controller' => 'main', 'action' => 'docs' )
-        ),
-        array(
-            'url'    => '/docs/api.html', 
-            'target' => array( 'controller' => 'main', 'action' => 'apidocs' ) 
-        ),
-        array(
-            'url'    => '/licence.html', 
-            'target' => array( 'controller' => 'main', 'action' => 'licence' ) 
-        ),
-        
-        # backend
-        array(
             'url'    => '/backend/login', 
             'target' => array( 'app' => 'backend', 'controller' => 'users', 'action' => 'login' ) 
         ),
